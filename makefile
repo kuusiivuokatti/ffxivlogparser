e997478cef58129c4710e6e0896b2a1f2912164c
kuusiivuokatti <105376916+kuusiivuokatti@users.noreply.github.com>
2022-10-20 20:09:36 +0300

    Added support for testing through makefile

--- makefile
+++ makefile
@@ -1,15 +1,29 @@
 CXX=g++
 CXXFLAG=-Wall -Wcast-align
+
+#PROD
 INCLUDE=
 OBJDIR=./src/
 OBJ=$(OBJDIR)*.cpp
 EXECDIR=./lib/
 EXECUTABLE=$(EXECDIR)ffxivlogparser.o
 
+#TEST
+TESTINCLUDE=
+TESTOBJDIR=./test/
+TESTOBJ=$(TESTOBJDIR)*.cpp
+TESTEXECDIR=./lib/
+TESTEXECUTABLE=$(EXECDIR)test.o
+
 all: $(EXECUTABLE)
 $(EXECUTABLE): $(OBJ)
 	mkdir -p $(EXECDIR)
 	$(CXX) $(CXXFLAG) $(INCLUDE) $(OBJ) -o $@
+
+test: $(TESTEXECUTABLE)
+$(TESTEXECUTABLE): $(TESTOBJ)
+	mkdir -p $(TESTEXECDIR)
+	$(CXX) $(CXXFLAG) $(TESTINCLUDE) $(TESTOBJ) -o $@
 
 clean:
 	rm -rf $(EXECDIR)
---
+++ test/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <filesystem>
+
+int main(){
+
+	std::error_code errCode;
+	bool canExit=false;
+	std::string logDir;
+
+	do{
+		std::cout<<"Enter log file path (q to exit)\n";
+		std::cin>>logDir;
+	
+		if(std::filesystem::is_directory(logDir,errCode)){
+			std::cout<<"Directory set to: "<<logDir<<'\n';
+			for(const std::filesystem::directory_entry& logFile:std::filesystem::directory_iterator(logDir)){
+				std::cout<<"Opening file: "<<logFile.path()<<'\n';
+			};
+		}else if(logDir=="q"){
+			std::cout<<"Exiting\n";
+			canExit=true;
+		}else{
+			std::cerr<<"Error while reading directory: "<<errCode.message()<<'\n';
+		};
+	}while(canExit==false);
+
+	return 0;
+
+};
